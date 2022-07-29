88737a2fb60304320ee21885c20f59956c8c20ac
Evgeny Berson <berson969@gmail.com>
2022-07-29 15:09:30 +0200

    Commit change

--- database.py
+++ database.py
@@ -22,6 +22,8 @@ class WhiteList(Base):
     user_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
     select_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
 
+    white_relation = relationship(Person, backref='whitelist')
+
 
 class BlackList(Base):
     __tablename__ = 'blacklist'
@@ -30,17 +32,24 @@ class BlackList(Base):
     user_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
     select_id = sq.Column(sq.Integer)
 
+    black_relation = relationship(Person, backref='blacklist')
+
 
 def create_database():
     load_dotenv()
     driver = 'postgresql'
     path_db = 'localhost:5432'
 
-    DNS = f"{driver}://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{path_db}/vkinder_db"
-    engine = sq.create_engine(DNS)
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    DSN = f"{driver}://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{path_db}/vkinder_db"
+    engine = sq.create_engine(DSN)
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(bind=engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    session.close()
+
+
+if __name__ == "__main__":
+    create_database()
