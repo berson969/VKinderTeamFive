63a8f6c5ce83b406ec149887b0089c481c9d22e8
Evgeny Berson <berson969@gmail.com>
2022-07-29 12:13:49 +0200

    Commit init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Работа на проектом курсовой  
+  
+* В файле `env_example` лежит шаблон для .env,  который надо заполнить своими данными  
+* `basic_code.py`  это шаблон для бота
+* database это модуль для открытия database
+* requirements.txt необходимые импортированные библиотеки `pip install -r requirements.txt`
---
+++ database.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = 'persons'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.Integer)
+    name = sq.Column(sq.String(length=60))
+    href = sq.Column(sq.String(length=60))
+
+
+class WhiteList(Base):
+    __tablename__ = 'whitelist'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
+    select_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
+
+
+class BlackList(Base):
+    __tablename__ = 'blacklist'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('persons.vk_id'), nullable=False)
+    select_id = sq.Column(sq.Integer)
+
+
+def create_database():
+    load_dotenv()
+    driver = 'postgresql'
+    path_db = 'localhost:5432'
+
+    DNS = f"{driver}://{os.getenv('USER')}:{os.getenv('PASSWORD')}@{path_db}/vkinder_db"
+    engine = sq.create_engine(DNS)
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
