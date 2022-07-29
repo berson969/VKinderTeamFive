ddb26adb90a0ef8558db77fe750d5b5d6898f9b6
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-07-29 17:32:31 +0300

    добавил токен

--- basic_code.py
+++ basic_code.py
@@ -3,7 +3,7 @@ from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = input('Token: ')
+token = input('Token: vk1.a.ANHGekoIKtlw7E7M8_EwtAry5mib578GD9r9iTlUUaHq7rx5UNh73gTavegLnVQbUStxFnhBsPd3xbxvQIQnF6_78rJZsT2g3-Q4h5CKLZb8Cti94yYODyVPoBPjkfGfYw9hqMYLiBbBWXhrSJlRlo93EZRJdt9hp9tjgh4ukVEUgx63eh5mOcsiQ-Ra4Ilo ')
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
