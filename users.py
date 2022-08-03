33d086543ff873af444d1fa78b038912f2a9a7a1
DubrovinMikhail <mixailgoo@gmail.com>
2022-08-03 22:24:27 +0300

    fiksed users.py

--- users.py
+++ users.py
@@ -3,12 +3,6 @@ import os
 from dotenv import load_dotenv
 from pprint import pprint
 
-def _count_likes(response1):
-    count_dict = {}
-    for i, item in enumerate(response1['response']['items']):
-        count_dict[i] = sum(item['likes'].values())
-    return sorted(count_dict, key=count_dict.get, reverse=True)
-
 
 class VK:
 
@@ -20,8 +14,7 @@ class VK:
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
-    def users_info(self, vk_id: str, length=3):
-        self.length = length
+    def users_info(self, vk_id: str):
         params = {'user_ids': vk_id, 'fields': 'bdate, sex, city'}
         response = requests.get(f'{self.URL}users.get', params={**self.params, **params}).json()
         # print(response)
@@ -36,35 +29,60 @@ class VK:
             if 'city' in response['response'][0].keys():
                 self.user_dict_info['city'] = response['response'][0]['city']['title']
             else:
-                self.user_dict_info['city'] = ''
+                self.user_dict_info['city'] = None
             if 'bdate' in response['response'][0].keys():
-                self.user_dict_info['birth_year'] = response['response'][0]['bdate']
+                if len(response['response'][0]['bdate'].split('.')) == 3:
+                    self.user_dict_info['birth_year'] = int(response['response'][0]['bdate'].split('.')[-1])
+                else:
+                    self.user_dict_info['birth_year'] = None
             else:
-                self.user_dict_info['birth_year'] = ''
+                self.user_dict_info['birth_year'] = None
             self.user_dict_info['sex'] = response['response'][0]['sex']
-        params1 = {'owner_id': vk_id, 'album_id': 'profile', 'extended': '1', 'photo_size': '1'}
-        response1 = requests.get(f'{self.URL}photos.get', params={**self.params, **params1}).json()
         try:
-            key_list = _count_likes(response1)
-            pprint(response1)
-            if len(key_list) < self.length:
-                self.length = len(key_list)
-            for i in range(self.length):
-                for result in response1['response']['items'][key_list[i]]['sizes']:
-                    if result['type'] == 'x':
-                        self.user_dict_info[f'photo{i}'] = result['url']
-                    elif result['type'] == 'y':
-                        self.user_dict_info[f'photo{i}'] = result['url']
-                    elif result['type'] == 'z':
-                        self.user_dict_info[f'photo{i}'] = result['url']
             return self.user_dict_info
         except KeyError:
             return self.user_dict_info
+
+    def search_users(self, birth_year, sex: int, sity: str):
+        '''
+        функция  выводит список пользователей с указанным годом рожнения, городом и полом
+        '''
+        user_list = []
+        #sex = [2, 1][sex-1] # эта строка меняет пол на противоположный,
+        # наверно надо вставить перед вызовом этой функии
+        # и еще может быть значение 0 когда пол не указан, тоже надо учесть
+        params = {'hometown': sity, 'birth_year': birth_year, 'sex': sex}
+        response = requests.get(f'{self.URL}users.search', params={**self.params, **params}).json()
+        for el in response['response']['items']:
+            user_info_dict = {'id': el['id'], 'name': el['first_name'] + ' ' + el['last_name']}
+            user_list.append(user_info_dict)
+        return user_list
+
+    def photos_get(self, vk_id):
+        '''функция выводит список словарей состоящих из ID фотографии
+        и количества лайков 3 самых популярных фотографий
+        '''
+        photo_list = []
+        params = {'owner_id': vk_id, 'album_id': 'profile', 'extended': '1', 'photo_size': '1'}
+        response = requests.get(f'{self.URL}photos.get', params={**self.params, **params}).json()
+        for item in response['response']['items']:
+            photo_dict = {'id': item['id'], 'likes': item['likes']['count']}
+            photo_list.append(photo_dict)
+
+        return sorted(photo_list, key=lambda d: d['likes'])[:3]
 
 
 if __name__ == '__main__':
     load_dotenv()
     access_token = os.getenv('TOKEN2')
     user = VK(access_token)
-    print(user.users_info(os.getenv('N_VK_ID')))
+    user_info = user.users_info('7312956')
+    (birth_year, sex, city) = (user_info['birth_year'], user_info['sex'], user_info['city'])
+    print(user.users_info('7312956'))
+    pprint(user.photos_get('7312956'))
+    sex = [2, 1][sex - 1]
+    pprint(user.search_users(birth_year, sex, city))
+
+
+
 
